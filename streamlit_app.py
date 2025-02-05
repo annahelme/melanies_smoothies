f6fe30fdea41dd573890cbe4469b5eb99ea9e559
Anna Helme <72913728+annahelme@users.noreply.github.com>
2025-02-05 08:42:02 +0100

    Update streamlit_app.py
    Add requests python function

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,11 @@ if ingredients_list:
     #st.stop()
     time_to_insert = st.button('Submit Order')
 
+# New Section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
